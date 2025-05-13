93f1e53568ac6bcafc7c2288d6eece183d8997fa
Luca Fantini <luca.fanta01@gmail.com>
2025-05-13 23:12:51 +0200

    Task 1 - Initial Structure

--- code/task_1/cooperative_localization.py
+++ code/task_1/cooperative_localization.py
@@ -1,1 +1,30 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+
+import gradient_tracking
+
+seed = 42
+np.random.seed(seed)
+
+
+# parameters
+N = 10
+Nt = 12
+
+# generate positions
+
+# generate noisy distances
+
+# define ell_i by using QQ and rr
+
+
+# Run multiple simulations
+#   call gradient_tracking(N, list: QQ, list: rr)
+
+# for each simulation:
+# - comparison with the "centralized" version
+# - plots: (log scale)
+#    - evolution of the "cost" function
+#    - norm of the TOTAL gradient of the cost function
--- code/task_1/gradient_tracking.py
+++ code/task_1/gradient_tracking.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+def cost_fcn(zz, QQ, rr):
+    pass
+
+def gradient_tracking():
+    # return cost, zz, ss
+    pass    
+
+if __name__ == "__main__":
+    # run set of simulations with different graphs (topology) with weights determined by Metropolis-Hasting.
+    # for each simulation:
+    # - comparison with the "centralized" version
+    # - plots: (log scale)
+    #    - evolution of the "cost"
+    #    - "consensus error"
+    #    - norm of the TOTAL gradient
+
+    pass
