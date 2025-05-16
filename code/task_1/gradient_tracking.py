Task 1 - Improved gradient tracking, still not correct!

code/task_1/gradient_tracking.py
@@ -3,7 +3,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 
-max_iter = 200
+seed = 42
+np.random.seed(seed)
+
+
+max_iter = 2000
 
 def quadratic_cost_fn(zz, QQ, rr):
     cost = 0.5 * zz.T @ QQ @ zz + rr.T @ zz
@@ -12,21 +16,31 @@ def quadratic_cost_fn(zz, QQ, rr):
 
 def gradient_tracking(NN, d, zz, ss, weighted_adj, cost_functions):
     cost = np.zeros((max_iter))
-    I_d = np.eye(d)
-    weighted_adj_ext = np.kron(weighted_adj, I_d)
+
+    print(zz[0].T)
+    print(zz[0].T.shape)
+    print("-"*35)
+    print(weighted_adj[0].T)
+    print(weighted_adj[0].T.shape)
+    print("-"*35)
+    print(zz[0].T @ weighted_adj[0].T)
+    print((zz[0].T @ weighted_adj[0].T).shape)
+
 
     for k in range(max_iter - 1):
         for i in range(NN):
-            zz[k+1, i] = weighted_adj_ext[i] @ zz[0].reshape(-1).T - alpha * ss[k, i]
-            consensus = weighted_adj_ext[i] @ ss[k].reshape(-1).T
+            zz[k+1, i] = zz[k].T @ weighted_adj[i].T - alpha * ss[k, i]
+            # zz[k+1, i] = (weighted_adj[i] @ zz[k]).T - alpha * ss[k, i]
+
+            consensus = ss[k].T @ weighted_adj[i].T
             
-            cost_ell_i_old, grad_ell_i_old = cost_functions[i](zz[k, i])
-            _, grad_ell_i_new = cost_functions[i](zz[k+1, i])
+            cost_k_i, grad_k_i = cost_functions[i](zz[k, i])
+            _, grad_k_plus_1_i = cost_functions[i](zz[k+1, i])
             
-            local_innovation = grad_ell_i_new - grad_ell_i_old
+            local_innovation = grad_k_plus_1_i - grad_k_i
             ss[k+1, i] = consensus + local_innovation
 
-            cost[k] += cost_ell_i_old
+            cost[k] += cost_k_i
 
     return cost, zz, ss
     
@@ -34,7 +48,7 @@ def gradient_tracking(NN, d, zz, ss, weighted_adj, cost_functions):
 def create_graph_iteratively(NN, p_er):
     ONES = np.ones((NN, NN))
     while 1:
-        G = nx.erdos_renyi_graph(NN, p_er)
+        G = nx.erdos_renyi_graph(NN, p_er, seed)
         Adj = nx.adjacency_matrix(G).toarray()
         test = np.linalg.matrix_power(Adj + np.eye(NN), NN)
 
@@ -46,7 +60,6 @@ def create_graph_iteratively(NN, p_er):
 
     while any(abs(np.sum(A, axis=1) - 1) > 1e-10):
         A = A / (A @ ONES)
-        
         A = A / (ONES.T @ A) # removing this part, I only get row-stochastcisty
         # they converge, but not at the minimum of the sum of the cost functions
         # but to a weighted avg of the sum
@@ -54,6 +67,59 @@ def create_graph_iteratively(NN, p_er):
         A = np.abs(A)
 
     return G, A
+
+def create_graph_birkhoff_von_neumann(NN, num_vertices):
+    I_n = np.eye(NN)
+    
+    # key=hash; value=np.ndarray
+    vertices = {}
+    
+    # Number of ways to choose k items from n items without repetition and with order.
+    max = math.perm(NN, NN)
+    print(f"Number of possibles vertices: {math.perm(NN, NN)}") # math.factorial(NN)
+    assert num_vertices <= max, "Vertices won't be unique"
+
+    # Ensure the presence of the Identity Matrix, self-loops
+    vertices[hash(I_n.tobytes())] = I_n
+
+    while(len(vertices) < num_vertices):
+        vertix = np.random.permutation(I_n)
+        vertix_hash = hash(vertix.tobytes())
+        
+        if vertix_hash not in vertices:
+            vertices[vertix_hash] = vertix
+    
+    # k=class_weights, the len(k) is the number of extracted numbers
+    class_weights = np.ones((num_vertices)) # equally distributed classes
+
+    convex_coefficients = np.random.dirichlet(alpha=class_weights, size=1).squeeze()
+    # print(f"convex_coefficients: {convex_coefficients}")
+
+    doubly_stochastic_matrix = np.zeros((NN, NN))
+    vertices = list(vertices.values())
+    # print(f"vertices: {vertices}")
+    
+    for i in range(num_vertices):
+        doubly_stochastic_matrix += vertices[i] * convex_coefficients[i]
+
+    # Ensure symmetry i.e. undirected graph
+    doubly_stochastic_matrix = (doubly_stochastic_matrix + doubly_stochastic_matrix.T) / 2
+
+    # print(doubly_stochastic_matrix)
+    # print(np.sum(doubly_stochastic_matrix, axis=0))
+    # print(np.sum(doubly_stochastic_matrix, axis=1))
+
+    paths_up_to_N = np.linalg.matrix_power(doubly_stochastic_matrix + np.eye(NN), NN)
+
+    # if is full, then in strongly connected
+    if np.all(paths_up_to_N > 0):
+        print("The graph is strongly connected")
+    else: 
+        print("The graph is NOT strongly connected!")
+
+    graph = nx.from_numpy_array(doubly_stochastic_matrix)
+
+    return graph, doubly_stochastic_matrix
 
 def show_graph_and_adj_matrix(graph, adj_matrix=None):
     fig, axs = plt.subplots(figsize=(6,3), nrows=1, ncols=2)
@@ -64,11 +130,11 @@ def show_graph_and_adj_matrix(graph, adj_matrix=None):
     
     cax = axs[1].matshow(adj_matrix, cmap='plasma')# , vmin=0, vmax=1)
     fig.colorbar(cax)
-    plt.show()
+    plt.show(block=False)
 
 
 if __name__ == "__main__":
-    NN = 5     # number of agents
+    NN = 10     # number of agents
     d = 2       # dimension of the state
     p_ER = 0.65
 
@@ -98,7 +164,7 @@ if __name__ == "__main__":
     # -------------------------------------
     # |   DISTRIBUTED GRADIENT TRACKING   |
     # -------------------------------------
-    alpha = 0.01
+    alpha = 0.05
 
     # two states
     z = np.zeros((max_iter, NN, d)) # indeces: [time, who, state-component]
@@ -110,11 +176,11 @@ if __name__ == "__main__":
 
     # init s
     for i in range(NN):
-        _, grad = quadratic_cost_fn(z[0,i], QQ_list[i], rr_list[i])
+        _, grad = quadratic_cost_fn(z[0, i], QQ_list[i], rr_list[i])
         s[0, i] = grad
 
 
-    graph, weighted_adj = create_graph_iteratively(NN, p_er=p_ER)
+    graph, weighted_adj = create_graph_birkhoff_von_neumann(NN, num_vertices=5)
     show_graph_and_adj_matrix(graph, weighted_adj)
 
     cost_functions = []
@@ -138,6 +204,23 @@ if __name__ == "__main__":
     for i in range(NN):
         # distance for each agent from the average of that iteration
         ax.plot(np.arange(max_iter), z[:, i] - z_avg)
+        
+    plt.show(block=False)
+
+    # graph config: [2]
+    # we can appreciate the LINEAR rate of convergence!
+    fig, axes = plt.subplots(figsize=(8, 6), nrows=1, ncols=2)
+
+    ax = axes[0]
+    ax.set_title("Cost error")
+    # optimal cost error - one line! we are minimizing the sum not each l_i
+    ax.semilogy(np.arange(max_iter - 1), np.abs(cost[:-1] - cost_opt))
+
+    ax = axes[1]
+    z_avg = np.mean(z, axis=1)
+    ax.set_title("Consensus error")
+    for i in range(NN):
+        ax.semilogy(np.arange(1, max_iter), np.abs(z[1:, i] - z_avg[1:])) # NOTE: skipping k=0
         
     plt.show()
 