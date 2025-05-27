c139ba55a11088f692bac3a59ae3055bba21c633
Luca Fantini <luca.fanta01@gmail.com>
2025-05-27 17:43:21 +0200

    Task 2 - Intial structure

---
+++ code/task_2/aggregative_tracking.py
@@ -0,0 +1,8 @@
+# implement of the aggregative tracking algorithm
+
+if __name__ == "__main__":
+    # setup 
+
+    # run simulations
+
+    pass
---
+++ code/task_2/team_behavior.py
@@ -0,0 +1,6 @@
+# setup the scenario of the agents with 2d-positions and private targets
+
+# run aggregative tracking
+
+# run animations
+# TODO: run animation after all computation or a real-time update for each iteration?
